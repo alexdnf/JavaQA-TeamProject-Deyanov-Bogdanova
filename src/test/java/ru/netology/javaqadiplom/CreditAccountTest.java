c62eb12b8e38e575b8e8808736ab5db54925b566
Alex Deyanov <deyanovan@yandex.ru>
2024-02-12 23:40:28 +0300

    Credit Tests Added

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -8,13 +8,70 @@ public class CreditAccountTest {
     @Test
     public void shouldAddToPositiveBalance() {
         CreditAccount account = new CreditAccount(
-                0,
+                1000,
                 5_000,
                 15
         );
 
         account.add(3_000);
 
-        Assertions.assertEquals(3_000, account.getBalance());
+        Assertions.assertEquals(4_000, account.getBalance());
+    }
+    @Test
+    public void shouldAddToNegativeBalance() {
+        CreditAccount account = new CreditAccount(
+                -1000,
+                5_000,
+                15
+        );
+
+        account.add(3_000);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+    @Test
+    public void shouldPayTest () {
+        CreditAccount account = new CreditAccount(1000, 5000, 15);
+
+        account.pay(3000);
+
+        Assertions.assertEquals(-2_000, account.getBalance());
+    }
+    @Test
+    public void shouldNotPayTest () {
+        CreditAccount account = new CreditAccount(1_000, 5_000, 15);
+
+        account.pay(10_000);
+
+        Assertions.assertEquals(1_000, account.getBalance());
+    }
+    @Test
+    public void yearChangeTestIfAccountIsPositive () {
+        CreditAccount account = new CreditAccount(1000, 5000, 15);
+
+
+        Assertions.assertEquals(0, account.yearChange());
+    }
+    @Test
+    public void yearChangeTestIfAccountIsNegative () {
+        CreditAccount account = new CreditAccount(-1000, 5000, 15);
+
+
+        Assertions.assertEquals(-150, account.yearChange());
+    }
+    @Test
+    public void creditAccountIfBalanceIsNegativeTest () {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {new CreditAccount(-1000, 5000, 15);});
+    }
+    @Test
+    public void creditAccountIfLimitIsNegativeTest () {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {new CreditAccount(1000, -5000, 15);});
+    }
+    @Test
+    public void creditAccountIfRateIsNegativeTest () {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {new CreditAccount(1000, 5000, -15);
+    });
     }
 }
